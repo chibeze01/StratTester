c52df2518fc3346710f981c4765257e238c27ef0
Chib master <chibeze2@hotmail.co.uk>
2024-10-13 21:43:19 +0100

    Deleted boating

--- backtester/config.py
+++
@@ -1,9 +0,0 @@
- 
-# backtester/config.py
-
-STRATEGY_EXE_PATH = 'strategy/dist/strategy.exe'
-WEBSOCKET_URI = 'ws://localhost:8765'
-
-# Trading parameters
-INITIAL_CAPITAL = 1000000  # Initial capital in USD
-TRANSACTION_COST = 0.0001  # Transaction cost per trade
--- run_backtester.py
+++
@@ -1,7 +0,0 @@
- 
-# run_backtester.py
-
-from backtester.backtester import main
-
-if __name__ == "__main__":
-    main()
